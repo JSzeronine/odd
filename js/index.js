1cb053438c52c97d48667936e37192f509625f03
Arden09 <kineage18@gmail.com>
2025-09-08 23:14:33 +0900

    css 수정

--- js/index.js
+++ js/index.js
@@ -304,18 +304,6 @@
 
                 gsap.to(harimList.eq(prevIndex), { opacity: 0, duration: 0.3, ease: Cubic.easeOut });
                 gsap.to(harimList.eq(currentIndex), { opacity: 1, duration: 0.3, ease: Cubic.easeOut });
-
-                gsap.fromTo(
-                    harimListText.eq(prevIndex),
-                    { x: 0, opacity: 1 },
-                    { x: 15, opacity: 0, duration: 0.3, ease: Cubic.easeOut }
-                );
-
-                gsap.fromTo(
-                    harimListText.eq(currentIndex),
-                    { x: -15, opacity: 0 },
-                    { x: 0, opacity: 1, duration: 0.3, ease: Cubic.easeOut }
-                );
             }, 2000);
         }
 
