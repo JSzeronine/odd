1289ca9baa71ea63325ea9698075789ed1f2c4a9
Arden09 <kineage18@gmail.com>
2025-09-16 21:37:48 +0900

    css 수정

--- js/sub.js
+++ js/sub.js
@@ -133,15 +133,16 @@
             const tutorialAnimationBx = $(".tutorial-animation--items");
             function showAnimation(i, isBoo ){
                 if( i === 4 ) return;
-
                 const animationBx = $( tutorialAnimationBx[i] );
-
                 gsap.killTweensOf( animationBx );
-
                 if( isBoo ){
-                    gsap.to( animationBx, { opacity: 1, ease: Cubic.easeOut } );
+                    gsap.to(animationBx, { 
+                        opacity: 1,  
+                        filter: "blur(0px)",
+                        ease: Cubic.easeOut 
+                    });
                 }else{
-                    gsap.to( animationBx, { opacity: 0, ease: Cubic.easeOut } );
+                    gsap.to( animationBx, { opacity: 0, filter: "blur(10px)", ease: Cubic.easeOut } );
                 }
             }
 
